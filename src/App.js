8e994d6ee04f591c61622d9b6e7fc6e701e998d6
abhisheksdm1 <81433509+abhisheksdm1@users.noreply.github.com>
2023-03-30 15:42:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './Components/Header';
+import SideBar from './Components/SideBar';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      {/* <SideBar/>   */}
+
+    </>
+  );
+}
+
+export default App;
